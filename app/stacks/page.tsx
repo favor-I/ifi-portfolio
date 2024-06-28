e080050a32a83abdb79fa5a0dc06793e39be7e28
Favour Ifeanacho <favour.franki3@gmail.com>
2024-06-28 13:23:17 +0000

    Installed next js and set up

---
+++ app/stacks/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Stacks</div>
+  )
+}
+
+export default page
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,74 +1,8 @@
-import React,{ useRef, useState, useEffect } from 'react'
-import { Flex, Text, Link } from '@chakra-ui/react'
-import { linkedIn, twitter } from '../assets/svg/Socials'
+import React from 'react'
 
-interface Props {
-  navREF: {
-    current: any
-  },
-  aboutREF: {
-    current: any
-  },
-  contactREF: {
-    current: any
-  },
-  serviceREF: {
-    current: any
-  },
-  projectREF: {
-    current: any
-  }
-}
-
-const Navbar:React.FC<Props> = ({navREF, aboutREF, contactREF, serviceREF, projectREF}) => {
-
-  const [bgColor, setBgColor] = useState('')
-  useEffect(() => {
-    window.addEventListener('scroll', handleNavBg)
-  }, [])
-  
-  const handleNavRef = () => {
-    navREF.current.scrollIntoView()
-    // console.log(navREF)
-  }
-  const handleAboutRef = () => {
-    aboutREF.current.scrollIntoView()
-    // console.log(aboutREF)
-  }
-  const handleConcactRef = () => {
-    contactREF.current.scrollIntoView()
-    // console.log(contactREF)
-  }
-  // const handleServicetRef = () => {
-  //   contactREF.current.scrollIntoView()
-  //   // console.log(contactREF)
-  // }
-  // const handleProjectRef = () => {
-  //   contactREF.current.scrollIntoView()
-  //   // console.log(contactREF)
-  // }
-  const handleNavBg = () => {
-    window.scrollY > 20 ? setBgColor('#F6F8FE'): bgColor
-    window.scrollY < 20 ? setBgColor(bgColor): bgColor
-    // console.log(window.scrollY)
-  }
-
+const Navbar = () => {
   return (
-    <>
-        <Flex width="full" bg={bgColor}  color="" position="fixed" paddingRight="80px" paddingLeft="100px"  height="5em" align="center">
-            <Link onClick={handleNavRef} _hover={{'cursor': 'pointer', 'color':'blue'}}><Text display="flex" fontWeight="800" fontFamily="" fontSize="20px" width="8em" colorScheme="" marginRight="">Favour<Text color="black">.I</Text></Text></Link>
-            <Flex  width="30em" marginRight="23em" justifyContent="space-between">
-                <Link onClick={handleAboutRef} _hover={{'cursor': 'pointer', 'color':'blue'}} width="">About Me</Link>
-                <Link _hover={{'cursor': 'pointer', 'color':'blue'}} width="">Services</Link>
-                <Link _hover={{'cursor': 'pointer', 'color':'blue'}} width="">Projects</Link>
-                <Link onClick={handleConcactRef} _hover={{'cursor': 'pointer', 'color':'blue'}} width=""><Text>Contact Me</Text></Link>
-            </Flex>
-            <Flex>
-              <Link href="https://www.twitter.com/iconictechie" isExternal><img src={twitter} alt="Twitter icon svg"/></Link>
-              <Link href="https://www.linkedin.com/in/ifeanacho-favour/" isExternal><img src={linkedIn} alt="Twitter icon svg"/></Link>
-            </Flex>
-        </Flex>
-    </>
+    <div></div>
   )
 }
 
